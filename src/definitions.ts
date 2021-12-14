6d279e41b944a0d4b39dd69e1c92fa3873525b0d
martindonadieu <martindonadieu@gmail.com>
2021-12-14 14:59:33 +0000

    fix: definition

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,10 +1,22 @@
+import type { PluginListenerHandle } from "@capacitor/core";
 export interface SpeechRecognitionPlugin {
   available(): Promise<{ available: boolean }>;
-  start(options?: UtteranceOptions): Promise<{ matches: String[] }>;
+  start(options?: UtteranceOptions): Promise<{ matches: string[] }>;
   stop(): Promise<void>;
   getSupportedLanguages(): Promise<{ languages: any[] }>;
   hasPermission(): Promise<{ permission: boolean }>;
   requestPermission(): Promise<void>;
+  /**
+   * Called when partialResults set to true and result received
+   *
+   * Provides partial result.
+   *
+   * @since 2.0.2
+   */
+  addListener(
+    eventName: "partialResults",
+    listenerFunc: (data: { matches: string[] }) => void
+  ): Promise<PluginListenerHandle> & PluginListenerHandle;
 }
 
 export interface UtteranceOptions {
