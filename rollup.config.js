f1864ac9884907fe3f92317b93e0bc4e39f2a2d5
jcesarmobile <jcesarmobile@gmail.com>
2024-06-06 19:18:25 +0200

    fix!: use correct name on rollup

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ export default {
     {
       file: 'dist/plugin.js',
       format: 'iife',
-      name: 'capacitorPrivacyScreen',
+      name: 'capacitorSpeechRecognition',
       globals: {
         '@capacitor/core': 'capacitorExports',
       },
